7b244c441d6da800912a381e8b6457b28127b6d9
Arear <119037037+Arear2004@users.noreply.github.com>
2023-04-21 13:32:36 +0300

    Add files via upload

---
+++ gd5/gd5/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace gd5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] mass = new int[10];
+            
+            Random random = new Random();
+            for (int i = 0; i < 10; i++)
+            {
+                mass[i] = random.Next(0, 10);
+            }
+            int[] massb = mass;
+            mass = mass.Reverse().ToArray();
+            mass = mass.Distinct().ToArray();
+            mass = mass.Reverse().ToArray();
+            Console.WriteLine(string.Join(", ", massb));
+            Console.WriteLine(string.Join(", ", mass)) ;
+        }
+    }
+}
